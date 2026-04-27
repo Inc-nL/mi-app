2e42a9a2c6dba0069b2f1e3e9f0baefb26729751
Inc-nL <sc.llunp@gmail.com>
2026-04-27 17:59:07 -0500

    Update program.cs

--- program.cs
+++ program.cs
@@ -5,7 +5,15 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hola Mundo");
+		Console.WriteLine("Hola Mundo");
+		Console.WriteLine("Hola Mundo");
+		Console.WriteLine("Hola Mundo");
+		Console.WriteLine("Hola Mundo");
+		Console.WriteLine("Hola Mundo");
+		Console.WriteLine("Hola Mundo");
+		Console.WriteLine("Hola Mundo");
         
+        // Mantener el programa vivo
         while (true)
         {
             System.Threading.Thread.Sleep(1000);
