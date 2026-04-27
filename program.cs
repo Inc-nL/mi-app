f0770a46d855aa03ed9ae7d108142ee40ca8da38
Inc-nL <sc.llunp@gmail.com>
2026-04-27 17:58:38 -0500

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hola Mundo");
+        
+        while (true)
+        {
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
